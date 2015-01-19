ffda5068042184197239beb8767590a857b3f093
DrMabuse23 <drmabuse@posteo.de>
2015-01-19 23:52:46 +0100

    start db connection

--- app/db/connection.js
+++ app/db/connection.js
@@ -1,20 +1,76 @@
 'use strict';
-var _accessToken = 'xx';
-var _email = 'xx';
-
-var _getToken = function () {
-  return _accessToken;
+var joi = require('joi');
+var jf = require('jsonfile');
+var util = require('util');
+var schema = require('../schema/open-ws');
+var _ = require('lodash');
+/**
+ * @type {string}
+ * @private
+ * @return {object}
+ */
+var _connectionJson = __dirname + '/../config/openws.json';
+/**
+ *
+ * @type {null}
+ * @private
+ *
+ */
+var _accessToken = null;
+/**
+ *
+ * @type {null}
+ * @private
+ */
+var _email = null;
+/**
+ *
+ * @type {null}
+ * here we store the connection information
+ */
+var dbConfiguration = null;
+/**
+ *
+ * @private
+ */
+var _readConfig = function () {
+  return jf.readFile(_connectionJson, function (err, obj) {
+    if (err) {
+      throw err;
+    }
+    return obj;
+  });
 };
-
-var _getEmail = function () {
-  return _email;
+/**
+ * @param {object} obj
+ * @private
+ */
+var _validateConfig = function (obj) {
+  return joi.validate(obj, schema, function (err, value) {
+    if (err) {
+      return err;
+    }
+    return value;
+  });
 };
-
-var getLoginCredentials = function () {
-  return {
-    token: _getToken(),
-    email: _getEmail()
+/**
+ *
+ */
+var _setConfig = function () {
+  var config = _readConfig();
+  var validate = _validateConfig(config);
+  if (_.isNull(validate)) {
+    _accessToken = config.access_token;
+    _email = config.email;
+    return config;
   }
 };
 
-module.exports = getLoginCredentials;
+module.exports = function () {
+  dbConfiguration = _setConfig();
+  if (_.isObject(dbConfiguration)) {
+    console.log(dbConfiguration);
+    return dbConfiguration;
+  }
+  return false;
+};
